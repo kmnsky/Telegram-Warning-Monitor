9669698f81bc505094d22ac7c5c414c3d8384d8f
Vlad <74014259+kmnsky@users.noreply.github.com>
2025-08-09 15:30:52 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# 🔐 Ваші облікові дані Telegram API
+api_id = 'your_api_id'
+api_hash = 'your_api_hash'
+phone = 'your_phone_number'
+
+# 📡 Джерело повідомлень
+source_group = 'https://t.me/war_monitor'  # або '@назва_каналу'
+
+# 🎯 Цільова група (chat_id)
+your_group = -1002611049131  # замініть на ID вашої групи
